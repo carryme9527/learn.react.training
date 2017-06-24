783d6a44aaa27ac5922192177865f894a5c6dad8
carryme9527 <carryme9527@users.noreply.github.com>
2017-06-24 22:20:46 +0800

    .map & .filter

--- app/index.js
+++ app/index.js
@@ -16,6 +16,43 @@ class App extends React.Component {
   }
 }
 
+class Users extends React.Component {
+  render() {
+    var friends = this.props.list.filter(function (user) {
+      return user.friend === true;
+    });
+    var nonFriends = this.props.list.filter(function (user) {
+      return user.friend !== true;
+    });
+
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map(function (user) {
+            return (
+              <li keye={user.name}>
+                {user.name}
+              </li>
+            )
+          })}
+        </ul>
+        <hr />
+        <h1>Non Friends</h1>
+        <ul>
+          {nonFriends.map(function (user) {
+            return (
+              <li keye={user.name}>
+                {user.name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
 class Badge extends React.Component {
   render() {
     return (
@@ -39,6 +76,14 @@ ReactDOM.render(
       username='tylermcginnis'
       img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
     />
+    <Users list={[
+      { name: 'Tyler', friend: true },
+      { name: 'Ryan', friend: true },
+      { name: 'Michael', friend: false },
+      { name: 'Jessica', friend: true },
+      { name: 'Mikenzi', friend: false },
+      { name: 'Dan', friend: false }
+    ]} />
   </div>,
   document.getElementById('app')
 );
