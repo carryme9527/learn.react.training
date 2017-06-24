5e1a90ff26703974ec4ba685b3c87fce744f6269
carryme9527 <carryme9527@users.noreply.github.com>
2017-06-25 02:04:27 +0800

    reset before

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
-// require('./index.css');
+require('./index.css');
 
 // state
 // lifecycle event
@@ -17,88 +16,7 @@ class App extends React.Component {
   }
 }
 
-class Users extends React.Component {
-  render() {
-    var friends = this.props.list.filter(function (user) {
-      return user.friend === true;
-    });
-    var nonFriends = this.props.list.filter(function (user) {
-      return user.friend !== true;
-    });
-
-    return (
-      <div>
-        <h1>Friends</h1>
-        <ul>
-          {friends.map(function (user) {
-            return (
-              <li keye={user.name}>
-                {user.name}
-              </li>
-            )
-          })}
-        </ul>
-        <hr />
-        <h1>Non Friends</h1>
-        <ul>
-          {nonFriends.map(function (user) {
-            return (
-              <li keye={user.name}>
-                {user.name}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
-
-Users.propTypes = {
-  // list: PropTypes.arrayOf(PropTypes.object)
-  list: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    friend: PropTypes.bool.isRequired
-  }))
-}
-
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <img
-          src={this.props.img}
-          alt='Avatar'
-          style={{width: 100, height: 100}}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
-      </div>
-    )
-  }
-}
-
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired
-}
-
 ReactDOM.render(
-  <div>
-    <Badge
-      name='Tyler McGinnis'
-      username='tylermcginnis'
-      img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
-    />
-    <Users list={[
-      { name: 'Tyler', friend: true },
-      { name: 'Ryan', friend: true },
-      { name: 'Michael', friend: false },
-      { name: 'Jessica', friend: true },
-      { name: 'Mikenzi', friend: false },
-      { name: 'Dan', friend: false }
-    ]} />
-  </div>,
+  <App />,
   document.getElementById('app')
 );
