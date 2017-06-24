625d469d3d9f008e4d5ad520f4bdff8f32574abf
carryme9527 <carryme9527@users.noreply.github.com>
2017-06-25 01:21:50 +0800

    PropTypes adv

--- app/index.js
+++ app/index.js
@@ -55,7 +55,11 @@ class Users extends React.Component {
 }
 
 Users.propTypes = {
-  list: PropTypes.array.isRequired
+  // list: PropTypes.arrayOf(PropTypes.object)
+  list: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    friend: PropTypes.bool.isRequired
+  }))
 }
 
 class Badge extends React.Component {
