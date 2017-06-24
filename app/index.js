f28bc440a27076a6f4196aa22659f43d47e3abf2
carryme9527 <carryme9527@users.noreply.github.com>
2017-06-24 22:04:44 +0800

    Dataflow with Props

--- app/index.js
+++ app/index.js
@@ -16,7 +16,27 @@ class App extends React.Component {
   }
 }
 
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <img
+          src={this.props.img}
+          alt='Avatar'
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <App />,
+  <Badge
+    name='Tyler McGinnis'
+    username='tylermcginnis'
+    img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+  />,
   document.getElementById('app')
 );
